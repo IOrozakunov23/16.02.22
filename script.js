59f2f909713307b08180bb7455474c127cf28efb
IOrozakunov23 <islamburunduk045>
2022-02-16 15:43:18 +0300

    added 2 files

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+// Focus-make input active
 document.querySelector('input[type="text"]')
- .addEventListener('focus',function () {
-  this.value = "Hello";
- });
- document.querySelector('input[type="text"]')
-    .addEventListener('blur', function () {
-      this.value = "Bye";
+    .addEventListener('focus', function() {
+        this.style.borderColor = "initial";
+    });
+// Lose focus
+document.querySelector('input[type="text"]')
+    .addEventListener('blur', function() {
+        if (!this.value) {
+            this.style.border = "red";
+        }
     });
 
-    
+// Blur - check if it is empty. If empty highlight borders red
+// Focus - remove red borders.
+document.querySelector('input[type="text"]')
+    .addEventListener('change', function() {
+        if (this.checked) {
+          alert('You are employed');
+        }
+        else {
+          alert('You are employed')
+        }
+    });
+
+
+// 1. Attach changed to checkbox
+// 2. If value is true output "You are emplaed" if false "You are unempled"
+
+
