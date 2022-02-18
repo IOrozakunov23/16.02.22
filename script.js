3deb8635818d027ca0a251a4adb0be903fdc3541
IOrozakunov23 <islamburunduk045>
2022-02-18 14:05:43 +0300

    added 2 files

--- script.js
+++ script.js
@@ -1,30 +1,87 @@
-// Focus-make input active
-document.querySelector('input[type="text"]')
-    .addEventListener('focus', function() {
-        this.style.borderColor = "initial";
-    });
-// Lose focus
-document.querySelector('input[type="text"]')
-    .addEventListener('blur', function() {
-        if (!this.value) {
-            this.style.border = "red";
-        }
-    });
+// document.querySelector('input[type="text"]')
+//   .addEventListener('focus', function () {
+//     this.className = "";
+//   });
+// document.querySelector('input[type="text"]')
+//   .addEventListener('blur', function () {
+//     if (!this.value) {
+//       this.className = "bad";
+//     }
+//     else {
+//       this.className = "good";
+//     }s
+//   });
 
-// Blur - check if it is empty. If empty highlight borders red
-// Focus - remove red borders.
-document.querySelector('input[type="text"]')
-    .addEventListener('change', function() {
-        if (this.checked) {
-          alert('You are employed');
-        }
-        else {
-          alert('You are employed')
-        }
-    });
+// document.querySelector('input[type="text"]')
+//   .addEventListener('change', function () {
+//     // alert('Changed');
+//   });
+
+// document.querySelector('input[type="checkbox"]')
+//   .addEventListener('change', function () {
+//     if (this.checked) {
+//       // alert('You are employed');
+//     }
+//     else {
+//       // alert('You are unemployed');
+//     }
+//   });
+
+// document.querySelector('select')
+//   .addEventListener('input', function () {
+//     if (this.value == 'Bishkek') {
+//       alert('Chui');
+//     }
+//     else if (this.value == 'Karakol') {
+//       alert('Issyk-Kul');
+//     }
+//     else if (this.value == 'Osh') {
+//       alert('Osh');
+//     }
+//     else if (this.value == 'Jalal-abad') {
+//       alert('Jalal-abad');
+//     }
+//     else if (this.value == 'Naryn') {
+//       alert('Naryn');
+//     }
+//     else if (this.value == 'Talas') {
+//       alert('Talas');
+//     }
+//     else if (this.value == 'Batken') {
+//       alert('Batken');
+//     }
+//   });
 
 
-// 1. Attach changed to checkbox
-// 2. If value is true output "You are emplaed" if false "You are unempled"
+// document.querySelector('input[type="text"]')
+//   .addEventListener('copy', function () {
+//     console.log('copy triggered');
+//   });
+// document.querySelector('input[type="text"]')
+//   .addEventListener('cut', function () {
+//     console.log('cut triggered');
+//   });
+// document.querySelector('input[type="text"]')
+//   .addEventListener('paste', function (event) {
+//     event.preventDefault();
+//     console.log('paste triggered');
+//   });
 
 
+// let radios = document.getElementsByName('gender');
+
+// document.querySelector('form')
+//   .addEventListener('submit', function (event) {
+//     event.preventDefault();
+
+//     alert(`${document.querySelector('input[type="radio"]:checked').nextSibling.textContent} was selected`);
+//   });
+
+// radios.forEach(function(radio) {
+//   radio.addEventListener('input', function() {
+//     //alert(`${this.id} was selected`);
+//   });
+// });
+
+// // 1. Attach changed to checkbox
+// // 2. If value is true output "You are employed" if false "You are unemployed"
